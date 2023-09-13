b936a03a8383949b856602899191fdd111f599e0
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-14 01:58:31 +0900

    素数判定の実装

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,19 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
 
 # TODO
+
+if a <= 1:
+    print(a,"は素数ではありません。")
+else:
+ prime_a = True
+ for i in range(1,a //2):
+    if a % (i + 1) == 0:
+      prime_a = False
+      break  
+    if prime_a == True:
+        print(a, "は素数です")
+        break
+    else:
+        print(a, "は素数ではありません")
+        break
