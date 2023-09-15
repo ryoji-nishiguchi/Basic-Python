f6188a7c323626eddd57c0d14a4b71969254ccc8
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-15 22:25:56 +0900

    Python入門の課題です。

--- pi.py
+++ pi.py
@@ -2,6 +2,4 @@ text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
-
-# TODO
-print(len(text))
+words = text.replace(" ",",") # スペースを削除し、単語と単語の間にカンマを入れる。
