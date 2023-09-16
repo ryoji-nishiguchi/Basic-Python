e61a649bd41dab0a14c0ba7df89e3bae8bfa7377
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-16 22:36:23 +0900

    pi.py 最初の状態に戻した。

--- pi.py
+++ pi.py
@@ -3,8 +3,5 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-word_list = text.split() # カンマで区切った単語リストを作成
+# TODO
 
-word_lengths_str = list(map(str, [len(word.strip(".,")) for word in word_list])) # 各単語の文字数を計算し、mapを使用して文字列のリストに変換
-result = ''.join(word_lengths_str) #',[]を削除し、リストを連結
-print(result)
