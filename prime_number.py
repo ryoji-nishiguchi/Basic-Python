59255cd9b03400ec553f0f440c00964f48c98956
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-16 20:18:12 +0900

    制御構文のコマンド

--- prime_number.py
+++ prime_number.py
@@ -1,19 +1,29 @@
 a = int(input("aの値を入力: "))
 b = int(input("bの値を入力: "))
 
-# TODO
+# aが素数であることを調べる。
 
 if a <= 1:
     print(a,"は素数ではありません。")
 else:
  prime_a = True
  for i in range(1,a //2):
-    if a % (i + 1) == 0:
+    if a % (i+1) == 0:
       prime_a = False
       break  
-    if prime_a == True:
+if prime_a == True:
         print(a, "は素数です")
-        break
-    else:
+else:
         print(a, "は素数ではありません")
-        break
+
+if b <= 1:
+    print(b,"は素数ではありません。")
+else:
+ prime_b = True
+ for i in range(1,b //2):
+    if b % (i + 1) == 0:
+      prime_b = False 
+ if prime_b == True:
+        print(b, "は素数です")
+ else:
+        print(b, "は素数ではありません")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,8 @@
-from math import sin
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+from math import sin,pi
+h = (pi/2)/100 #問題文に基づいて考えた。 
+k = 1
+s = 0 #初期状態を定義 
+while k <= 100:
+ s += (h/2)*(sin((k-1)*h)+sin(k*h)) #問題文にあったaの値は0であるから省略した。
+ k = k + 1
+print(s)  
