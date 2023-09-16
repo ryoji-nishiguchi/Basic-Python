376e3e3056d3a67b3f57f0816ff3d431ea6a5806
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-16 21:16:29 +0900

    conflict解消

--- pi.py
+++ pi.py
@@ -6,6 +6,5 @@ text = """
 word_list = text.split() # カンマで区切った単語リストを作成
 
 word_lengths_str = list(map(str, [len(word.strip(".,")) for word in word_list])) # 各単語の文字数を計算し、mapを使用して文字列のリストに変換
-
-result = ''.join(word_lengths_str) #',[]を削除。
+result = ''.join(word_lengths_str) #',[]を削除し、リストを連結
 print(result)
