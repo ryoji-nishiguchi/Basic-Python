cd8e73c09c5c6700af822edc3c9667906cdbd8e6
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-09-20 01:03:21 +0900

    prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,19 +1,4 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+a = input("aの値を入力: ")
+b = input("bの値を入力: ")
 
-def judging_prime(c): # cが素数であることを調べる。
-    if c <= 1:
-     print(c,"は素数ではありません。")
-    else:
-     prime_c = True
-     for i in range(1,c //2):
-      if c % (i+1) == 0:
-        prime_c = False
-      break  
-     if prime_c == True:
-        print(c, "は素数です。")
-     else:
-        print(c, "は素数ではありません。")
-    
-judging_prime(a)
-judging_prime(b)
+# TODO
