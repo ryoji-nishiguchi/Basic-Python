d737fb157aeaf290ae5efa423e73d790f8b32b08
Ryoji Nishiguchi/西口凌司 <141410895+ryoji-nishiguchi@users.noreply.github.com>
2023-10-18 15:37:32 +0900

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,20 +1,19 @@
-n = input('自然数を入力してください。')
-try: #ここではtry-except文を用いる。
-    n = int(n)  # ユーザー入力を整数に変換
-    if n <= 0:
-        print('入力された数は自然数でない。')
+import math
+n = input('自然数を入力してください.')
+def judge_prime(x):
+    if not x.isdigit():  # nが整数であることの真偽
+        return '入力された数は自然数でありません。'
+    x = int(x)  # 文字列を整数に変換
+    if x <= 0:
+        return '入力された数は自然数でありません。'
     else:
         i = 2
-        while i < n:
-           if n%i == 0:
-              print('nは素数でない。')
-              break
-           elif i < n:
-              i += 1
-        else: 
-              print('nは素数である。')
-except ValueError:
-    print('入力された数は自然数でない。')
-    
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+        while i < x:
+            if x % i == 0:
+                return 'nは素数でない。'
+            else:
+                i += 1
+        else:
+            return 'nは素数である。'
+
+print(judge_prime(n))
